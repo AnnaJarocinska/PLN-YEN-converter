bb24ae52bbe0ade8fef36b9a0f0e595d9e7563b7
Anna Jarocińska <anna.jarocinska5@gmail.com>
2019-09-15 23:16:44 +0200

    add type = number to input

---
+++ src/components/CurrencyInput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class CurrencyInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.props.onCurrencyChange(e.target.value);
+    }
+
+    render() {
+        const value = this.props.value;
+        const currency = this.props.currency;
+        return (
+            <>
+                <fieldset className="currencyConverter__fieldset">
+                    <legend className="currencyConverter__legend">currency:{currency} </legend>
+                    <input type="number" className="currencyConverter__input" value={value} onChange={this.handleChange} />
+                </fieldset>
+            </>
+        );
+    }
+}
+
+export default CurrencyInput;
